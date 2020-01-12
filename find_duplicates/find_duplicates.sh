53276305cc8b63ba359129778a1de0f2b5aab8d4
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-01-12 13:50:14 +0100

    Fixing CodeFactor Issue

--- find_duplicates/find_duplicates.sh
+++ find_duplicates/find_duplicates.sh
@@ -7,7 +7,7 @@ readonly SEARCH_PATH="/SEARCH_PATH/"
 readonly TEMP_FILE=myTempfileName
 find ${SEARCH_PATH} -type f  > ${TEMP_FILE}
 < ${TEMP_FILE} sed 's_.*/__' | sort | uniq -d |
-while read file_name; do
+while read -r file_name; do
   grep "${file_name}" ${TEMP_FILE}
 done
 rm -f ${TEMP_FILE}
